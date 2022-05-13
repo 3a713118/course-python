44b656e8b67b74d636b75d5a03405f9689039a9c
3a713118 <79616549+3a713118@users.noreply.github.com>
2022-05-13 20:33:25 +0800

    python_add_20220513
    def add(x):
      a=1
      if x<=a:
        return
      if x>=a:
        b=0
        for a in range(x):
          s=x-a
          print(s,'+',)
          b=s+b
        return b
    z=add(10)
    print(z)

---
+++ myfun_add.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def add(x):
+  a=1
+  if x<=a:
+    return
+  if x>=a:
+    b=0
+    for a in range(x):
+      s=x-a
+      #print(s,'+',)
+      b=s+b
+    return b
+z=add(10)
+print(z)
